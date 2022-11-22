7fe69f421c3956c941a4098a47f81662d8ccb794
Guilherme Barros <barrosg70@yahoo.com>
2022-11-22 10:36:13 -0300

    versao 3 - gerar pdf ordem atrasada completo

--- gerencia/gerar_pdf_ordens_atr.py
+++ gerencia/gerar_pdf_ordens_atr.py
@@ -19,10 +19,46 @@ def gerar_pdf(orcamentos):
                 f"N° {orcamento.numero} - Data entrega: {orcamento.dt_entregue} - Tipo: {orcamento.tipo} - Secretaria: {orcamento.secretaria.nome} - Valor: R${orcamento.valor}"
             ],
         )
+    if len(dados) == 0:
+        dados.append(["SEM ORÇAMENTOS COM ORDEM DE SERVIÇO ATRASADA"])
+    dados.insert(0, ["ORÇAMENTOS SEM ORDEM DE SERVIÇO"])
     pdf = SimpleDocTemplate(fileName, pagesize=A4)
 
     tabela = Table(dados)
+
+    style = TableStyle(
+        [
+            ("BACKGROUND", (0, 0), (3, 0), colors.green),
+            ("TEXTCOLOR", (0, 0), (-1, 0), colors.whitesmoke),
+            ("ALIGN", (0, 0), (-1, -1), "CENTER"),
+            ("FONTNAME", (0, 0), (-1, 0), "Courier-Bold"),
+            ("FONTSIZE", (0, 0), (-1, 0), 14),
+            ("BOTTOMPADDING", (0, 0), (-1, 0), 12),
+            ("BACKGROUND", (0, 1), (-1, -1), colors.beige),
+        ]
+    )
+    tabela.setStyle(style)
+
+    rowNumb = len(dados)
+    for i in range(1, rowNumb):
+        if i % 2 == 0:
+            bc = colors.burlywood
+        else:
+            bc = colors.beige
+
+        ts = TableStyle([("BACKGROUND", (0, i), (-1, i), bc)])
+        tabela.setStyle(ts)
+
+    ts = TableStyle(
+        [
+            ("BOX", (0, 0), (-1, -1), 2, colors.black),
+            ("LINEBEFORE", (2, 1), (2, -1), 2, colors.red),
+            ("LINEABOVE", (0, 2), (-1, 2), 2, colors.green),
+            ("GRID", (0, 1), (-1, -1), 2, colors.black),
+        ]
+    )
+    tabela.setStyle(ts)
     elems = []
     elems.append(tabela)
-
+    pdf.title = "orçamentos ordem faltando"
     pdf.build(elems)
